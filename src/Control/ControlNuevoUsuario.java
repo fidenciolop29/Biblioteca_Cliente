5e8606d38eb9f73eeb247f8c27d2ba2283fce07d
fidea <fidea@192.168.1.73>
2022-06-05 13:48:01 -0500

    actualizacion nueva

--- src/Control/ControlNuevoUsuario.java
+++ src/Control/ControlNuevoUsuario.java
@@ -1,28 +1,50 @@
 
 package Control;
 
+import Modelo.ConsultasUsers;
 import Modelo.Usuario;
 import Vista.NuevoUsuario;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class ControlNuevoUsuario implements ActionListener {
     private NuevoUsuario vista;
     private Usuario mod;
+    private ConsultasUsers consulta;
+    private Control_Login controlusu;
     
-    public ControlNuevoUsuario(NuevoUsuario vista, Usuario mod){
+    public ControlNuevoUsuario(NuevoUsuario vista, Usuario mod, ConsultasUsers consulta,Control_Login controlusu){
         this.vista=vista;
         this.mod=mod;
         this.vista.agregarUser.addActionListener(this);
+        this.consulta=consulta;
+        this.controlusu=controlusu;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-       
+       if(e.getSource()==vista.agregarUser){
+           mod.setUser(vista.txfUsuario.getText());
+           mod.setPassword(vista.txfContra.getText());
+           if(consulta.registrar(mod))
+            {
+                JOptionPane.showMessageDialog(null, "registro guardado");
+                limpiar();
+                controlusu.iniciar();
+                vista.setVisible(false);
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Error al guardar");
+                limpiar();
+            }
+       }
     }
 
     public void iniciar(){
-        
+        vista.setVisible(true);
+        vista.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
     }
     public NuevoUsuario getVista() {
         return vista;
@@ -40,6 +62,10 @@ public class ControlNuevoUsuario implements ActionListener {
         this.mod = mod;
     }
     
+    public void limpiar(){
+        vista.txfContra.setText("");
+        vista.txfUsuario.setText("");
+    }
     
     
 }
