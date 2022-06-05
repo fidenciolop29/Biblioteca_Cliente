e9ecb10d5ec9bf91ad19e5aef829abb4bc17e6a3
fidea <fidea@192.168.1.73>
2022-06-05 13:06:41 -0500

    Nueva actualizacion, sorry voy lento

--- src/Control/ControlNuevoUsuario.java
+++ src/Control/ControlNuevoUsuario.java
@@ -1,6 +1,45 @@
 
 package Control;
 
-public class ControlNuevoUsuario {
+import Modelo.Usuario;
+import Vista.NuevoUsuario;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ControlNuevoUsuario implements ActionListener {
+    private NuevoUsuario vista;
+    private Usuario mod;
+    
+    public ControlNuevoUsuario(NuevoUsuario vista, Usuario mod){
+        this.vista=vista;
+        this.mod=mod;
+        this.vista.agregarUser.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       
+    }
+
+    public void iniciar(){
+        
+    }
+    public NuevoUsuario getVista() {
+        return vista;
+    }
+
+    public Usuario getMod() {
+        return mod;
+    }
+
+    public void setVista(NuevoUsuario vista) {
+        this.vista = vista;
+    }
+
+    public void setMod(Usuario mod) {
+        this.mod = mod;
+    }
+    
+    
     
 }
